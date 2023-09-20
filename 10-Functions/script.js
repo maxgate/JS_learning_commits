3bc72f9edc050b9eb35958fb2b0dddefa31e4ca5
max vincent <kelechivincy@gmail.com>
2023-09-20 07:32:27 -0700

    functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -216,6 +216,7 @@ const addVat = addTaxx(0.1);
 console.log(addVat(200));
 */
 
+/*
 /////////////////////////////////////////
 // Coding Challenge
 
@@ -258,3 +259,106 @@ document
 poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+*/
+
+/*
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+//IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+// using Arrow function
+(() => {
+  console.log('This will never run again');
+})();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 34;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+*/
+/*
+/////////////////////////////////////////
+// Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passenger`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+// more Closure exammples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 24;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+/////////////////////////////////////////
+// Coding Challenge #2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
