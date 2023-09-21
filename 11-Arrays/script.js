4d3f358797aec6d1e9fffe89c08cfac4d74e549a
max vincent <kelechivincy@gmail.com>
2023-09-21 10:14:23 -0700

    array

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -2,13 +2,7 @@
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /*
 //////////////////////////////////////////////////
@@ -47,7 +41,7 @@ console.log(...arr, ...arr2);
 // JOIN
  console.log(letters.join('  '));
 */
-
+/*
 // AT method
 const arr = [23, 11, 64];
 console.log(arr[0]);
@@ -56,4 +50,53 @@ console.log(arr.at(0));
 // getting last array element
 console.log(arr[arr.length - 1]);
 console.log(arr.slice(-1)[0]);
-console.log(arr.at(0));
+console.log(arr.at(-1));
+// the AT method works also on strings
+console.log("Vincent".at(0));
+console.log("Vincent".at(-1));
+*/
+
+/*
+///////////////////////////////////////////
+// FOREACH METHOD ON ARRAYS
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log("-----FOR OF -----");
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(` Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log("-------- FOREACH -----");
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+*/
+
+///////////////////////////////////////////
+// FOREACH METHOD ON MAPS
+
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}, ${value}`);
+});
+
+// FOREACH METHOD ON SETS
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
