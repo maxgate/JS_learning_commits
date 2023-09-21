056e5084b18455940b202949e9135ea1352d4776
max vincent <kelechivincy@gmail.com>
2023-09-21 06:03:47 -0700

    index file

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -3,10 +3,57 @@
 // LECTURES
 
 const currencies = new Map([
- ['USD', 'United States dollar'],
-   ['EUR', 'Euro'],
-     ['GBP', 'Pound sterling'],
-     ]);
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
 
-     const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-    
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/*
+//////////////////////////////////////////////////
+// Array Methods
+let arr = ["a", "b", "c", "d", "e"];
+
+// SLICE
+console.log(arr.slice());
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice(1, -1));
+console.log([...arr]);
+
+// SPLICE - deletes - MUTATES THE ORIGINAL ARRAY
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE - Mutates the orginal array
+arr = ["a", "b", "c", "d", "e"];
+const arr2 = ["j", "i", "h", "g", "f"];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+// the same like below
+console.log(...arr, ...arr2);
+
+// JOIN
+ console.log(letters.join('  '));
+*/
+
+// AT method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(0));
