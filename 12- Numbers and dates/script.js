1e46b7203a0aa0b2eb844c8e29a51fda00878932
max vincent <kelechivincy@gmail.com>
2023-10-03 16:07:50 -0700

    dates

--- 12- Numbers and dates/script.js
+++ 12- Numbers and dates/script.js
@@ -190,8 +190,59 @@ console.log(10 / 3);
 ////////////////////////////////////////////
 // CREATING DATES
 
+/*
 // creat a date
 const now = new Date();
 console.log(now);
 
+console.log(new Date("Aug 02 2020 18:05:41"));
+console.log(new Date("December 24, 2015"));
+
+console.log(2037, 10, 19, 15, 23, 5);
+console.log(2037, 10, 31);
+
 console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+*/
+
+/*
+///////////////////////////////////////////
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.toDateString());
+// geting Time stamp
+console.log(future.getTime());
+
+console.log(new Date(2142285780000));
+
+// geting current time stamp
+console.log(Date.now());
+console.log(new Date(2121114180000));
+
+// date set method
+future.setFullYear(2034);
+future.setHours(13)
+console.log(future);
+*/
+//////////////////////////////////////////
+// OPERATIONS WITH DATES
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date2) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
+
+/////////////////////////////////////////////////
+// INTERNATIONALIZING DATES (INTL)
