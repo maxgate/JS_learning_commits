1ebad0f55f1d9cf2b7a11e388ec0d5b1b7fe1d6a
max vincent <kelechivincy@gmail.com>
2023-10-16 16:50:51 -0700

    oop

--- OOP/script.js
+++ OOP/script.js
@@ -1,1 +1,161 @@
 'use strict';
+
+/*
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //   // never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const max = new Person('max', 1991);
+console.log(max);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automaticall return {}
+
+const pinky = new Person('Pinky', 2004);
+const james = new Person('James', 2000);
+console.log(pinky, james);
+
+console.log(max instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+max.calcAge();
+pinky.calcAge();
+
+console.log(max.__proto__);
+console.log(max.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(max));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .PrototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(max.species, pinky.species);
+
+console.log(max.hasOwnProperty('firstName'));
+console.log(max.hasOwnProperty('species'));
+
+console.log(max.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(max.__proto__.__proto__);
+console.log(max.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+*/
+
+/*
+//////////////////////////////////////////////
+// Coding Challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake(); 
+bmw.accelerate();
+*/
+
+/*
+/////////////////////////////////////
+// ES6 CLASSES
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// points to NOTE about CLASSES
+// 1. Classes are NOT hoisted
+// 2. Classes are executed in strict mode
+// 3. Classes are first-class citizens
+*/
+
+//////////////////////// /////////////////
+// SETTERS AND GETTERS
+
+const account = {
+  owner: 'jonas',
+  movement: [200, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
